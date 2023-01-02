56a6801194b3b7fcceea819ba7f921b0405d060b
bagumka81 <bagumka@gmail.com>
2023-01-03 01:11:54 +0300

    finished Task 1

--- bin/dart_basics.dart
+++ bin/dart_basics.dart
@@ -1,5 +1,15 @@
 import 'package:dart_basics/dart_basics.dart' as dart_basics;
+import 'package:dart_basics/delimeters_calculator.dart' as delimeters_calculator;
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_basics.calculate()}!');
+
+  print('------- Task 1 -------');
+  const int num1 = 10;
+  const int num2 = 15;
+  print('greatest common divisor for $num1 and $num2 is '
+  '${delimeters_calculator.DelimetersCalculator.gcd(num1, num2)}');
+  print('least common multiple for $num1 and $num2 is '
+      '${delimeters_calculator.DelimetersCalculator.lcm(num1, num2)}\n');
+
 }
---
+++ lib/delimeters_calculator.dart
@@ -0,0 +1,26 @@
+///
+///
+class DelimetersCalculator {
+  ///Finds greatest common divisor for
+  /// two numbers [num1] and [num2]
+  static int gcd(int num1, int num2) {
+    if (num1 < 0 || num2 < 0) {
+      throw ArgumentError("lcm arguments must be >=0");
+    }
+    if (num1 < num2) {
+      int temp = num1;
+      num1 = num2;
+      num2 = temp;
+    }
+    return num2 > 0 ? gcd(num2, num1 % num2) : num1;
+  }
+
+  ///Finds least common multiple for
+  /// two numbers [num1] and [num2]
+  static int lcm(int num1, int num2) {
+    if (num1 <= 0 || num2 <= 0) {
+      throw ArgumentError("lcm arguments must be >0");
+    }
+    return (num1 / gcd(num1, num2)).toInt() * num2;
+  }
+}
